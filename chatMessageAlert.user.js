2faf4db7e33f9be1e0e6d1cfa378f69089f31b0c
TheKeineAhnung <fireball73@gmx.de>
2021-06-01 13:25:35 +0200

    🐛 fixed play bug

--- chatMessageAlert.user.js
+++ chatMessageAlert.user.js
@@ -4,6 +4,7 @@
 // @version      1.0
 // @description  Alert when new Message is in the Chat
 // @author       KeineAhnung
+// @run-at       document-end
 // @include      https://rettungssimulator.online/
 // @updateURL    https://github.com/TheKeineAhnung/ReSi-Scripte/raw/Develop/chatMessageAlert.user.js
 // @downloadURL  https://github.com/TheKeineAhnung/ReSi-Scripte/raw/Develop/chatMessageAlert.user.js
@@ -11,21 +12,21 @@
 
 window.onload = async function main() {
     var messagesSinceReload = 0
-    var allMessages = document.querySelectorAll(".message")
-    var targetDiv = document.querySelector('.messages')
-    const config = { childList: true, subtree: true }
-    var audio = new Audio('')
+    var allMessages = document.querySelectorAll(".message");
+    var targetDiv = document.querySelector('.messages');
+    const config = { childList: true, subtree: true };
+    var audio = new Audio('');
 
     const observer = new MutationObserver(function(mutations) {
         var newestMessage;
         var allMessages = document.querySelectorAll(".message");
         for (var i of allMessages) {
-            var newestMessageClass = i.classList
+            var newestMessageClass = i.classList;
         }
         if (newestMessageClass == "message" && messagesSinceReload != 0) {
             audio.play();
         } else {}
         messagesSinceReload++;
     })
-    observer.observe(targetDiv, config)
+    observer.observe(targetDiv, config);
 }
