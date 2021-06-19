c7557eae9b424769e1cef8aebfc0e2f40667e24d
TheKeineAhnung <fireball73@gmx.de>
2021-06-19 19:04:32 +0200

    🐛 fixed building size

--- customBuildingIcons.user.js
+++ customBuildingIcons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Custom Building Icons
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @run-at       document-end
 // @description  customize your building Icons
 // @author       KeineAhnung
@@ -27,18 +27,32 @@ window.onload = async function main() {
         var actualImage = images[i];
         if (actualImage.src == 'https://rettungssimulator.online/images/marker/departments/fireDepartment.png' && sessionStorage.getItem('Feuerwache') != 'null') {
             actualImage.src = sessionStorage.getItem('Feuerwache');
+            actualImage.style.width = "auto";
+            actualImage.style.style.height = "auto";
         } else if (actualImage.src == 'https://rettungssimulator.online/images/marker/departments/fireSchool.png' && sessionStorage.getItem('Feuerwehrschule') != 'null') {
             actualImage.src = sessionStorage.getItem('Feuerwehrschule');
+            actualImage.style.width = "auto";
+            actualImage.style.height = "auto";
         } else if (actualImage.src == 'https://rettungssimulator.online/images/marker/departments/emsDepartment.png' && sessionStorage.getItem('Rettungswache') != 'null') {
             actualImage.src = sessionStorage.getItem('Rettungswache');
+            actualImage.style.width = "auto";
+            actualImage.style.height = "auto";
         } else if (actualImage.src == 'https://rettungssimulator.online/images/marker/departments/hospital.png' && sessionStorage.getItem('Krankenhaus') != 'null') {
             actualImage.src = sessionStorage.getItem('Krankenhaus');
+            actualImage.style.width = "auto";
+            actualImage.style.height = "auto";
         } else if (actualImage.src == 'https://rettungssimulator.online/images/marker/departments/policeDepartment.png' && sessionStorage.getItem('Polizeiwache') != 'null') {
             actualImage.src = sessionStorage.getItem('Polizeiwache');
+            actualImage.style.width = "auto";
+            actualImage.style.height = "auto";
         } else if (actualImage.src == 'https://rettungssimulator.online/images/marker/departments/policeSchool.png' && sessionStorage.getItem('Polizeischule') != 'null') {
             actualImage.src = sessionStorage.getItem('Polizeischule');
+            actualImage.style.width = "auto";
+            actualImage.style.height = "auto";
         } else if (actualImage.src == 'https://rettungssimulator.online/images/marker/departments/controlCenter.png' && sessionStorage.getItem('Leitstelle') != 'null') {
             actualImage.src = sessionStorage.getItem('Leitstelle');
+            actualImage.style.width = "auto";
+            actualImage.style.height = "auto";
         }
     }
 }
