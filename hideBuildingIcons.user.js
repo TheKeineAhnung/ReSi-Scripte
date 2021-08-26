df313596be7d04a740be7b587e530172ae3073b7
TheKeineAhnung <fireball73@gmx.de>
2021-08-26 16:45:58 +0200

    💾 optimized code

--- hideBuildingIcons.user.js
+++ hideBuildingIcons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hide Building Icons
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @run-at       document-end
 // @description  Hides the building Icons on the map
 // @author       KeineAhnung
@@ -12,20 +12,28 @@
 // ==/UserScript==
 
 window.onload = async function main() {
-    var iconsToRemove = ["https://rettungssimulator.online/images/marker/departments/hospital.png", "https://rettungssimulator.online/images/marker/departments/emsDepartment.png", "https://rettungssimulator.online/images/marker/departments/controlCenter.png", "https://rettungssimulator.online/images/marker/departments/fireDepartment.png", "https://rettungssimulator.online/images/marker/departments/fireSchool.png", "https://rettungssimulator.online/images/marker/departments/policeDepartment.png", "https://rettungssimulator.online/images/marker/departments/policeSchool.png"]
-    var imagesArea = document.getElementsByClassName('leaflet-marker-pane');
-    var images = document.getElementsByTagName('img');
-    for (var i in images) {
-        var actualImage = images[i];
-        for (var actualIcon in iconsToRemove) {
-            if (actualImage == undefined) {
-                break;
-            }
-            if (actualImage.src == iconsToRemove[actualIcon]) {
-                actualImage.src = 'https://raw.githubusercontent.com/TheKeineAhnung/ReSi-Scripte/main/img/transparentPixel.png';
-                break;
-            }
-        }
-        i++;
+  var iconsToRemove = [
+    "https://rettungssimulator.online/images/marker/departments/hospital.png",
+    "https://rettungssimulator.online/images/marker/departments/emsDepartment.png",
+    "https://rettungssimulator.online/images/marker/departments/controlCenter.png",
+    "https://rettungssimulator.online/images/marker/departments/fireDepartment.png",
+    "https://rettungssimulator.online/images/marker/departments/fireSchool.png",
+    "https://rettungssimulator.online/images/marker/departments/policeDepartment.png",
+    "https://rettungssimulator.online/images/marker/departments/policeSchool.png",
+  ];
+  var images = document.getElementsByTagName("img");
+  for (var i in images) {
+    var actualImage = images[i];
+    for (var actualIcon in iconsToRemove) {
+      if (actualImage == undefined) {
+        break;
+      }
+      if (actualImage.src == iconsToRemove[actualIcon]) {
+        actualImage.src =
+          "https://raw.githubusercontent.com/TheKeineAhnung/ReSi-Scripte/main/img/transparentPixel.png";
+        break;
+      }
     }
-}
+    i++;
+  }
+};
