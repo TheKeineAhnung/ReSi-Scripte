40866a9a62265b0b7388f0ce900101dc33d2cbf9
TheKeineAhnung <fireball73@gmx.de>
2021-08-29 15:51:24 +0200

    ✨ added script

---
+++ customMissionIcons.user.js
@@ -0,0 +1,81 @@
+// ==UserScript==
+// @name         Custom Mission Icons
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @run-at       document-end
+// @description  customize your mission Icons
+// @author       KeineAhnung
+// @include      https://rettungssimulator.online
+// @include      https://rettungssimulator.online/*
+// @updateURL    https://github.com/TheKeineAhnung/ReSi-Scripte/raw/main/customMissionIcons.user.js
+// @downloadURL  https://github.com/TheKeineAhnung/ReSi-Scripte/raw/main/customMissionIcons.user.js
+// @icon         https://www.google.com/s2/favicons?domain=rettungssimulator.online
+// @grant        none
+// ==/UserScript==
+
+//1 = keine Fahrzeuge alamiert
+//2 = Auf anfahrt
+//3 = wird bearbeitet
+
+const config = `[
+    {
+      "name": "fire",
+      "1": "",
+      "2": "",
+      "3": ""
+    }
+  ]
+  `;
+const config_parse = JSON.parse(config);
+
+function replaceIcons(icon = "fire") {
+  for (var i in config_parse) {
+    var oneMap = document.querySelectorAll(
+      `img[src='images/marker/missions/${config_parse[i]["name"]}_1.png']`
+    );
+    var twoMap = document.querySelectorAll(
+      `img[src='images/marker/missions/${config_parse[i]["name"]}_2.png']`
+    );
+    var threeMap = document.querySelectorAll(
+      `img[src='images/marker/missions/${config_parse[i]["name"]}_3.png']`
+    );
+    for (var e in oneMap) {
+      console.log(oneMap[e].src);
+      if (
+        oneMap[e].src ===
+        `https://rettungssimulator.online/images/marker/missions/${icon}_1.png`
+      ) {
+        oneMap[e].src = `${config_parse[i]["1"]}`;
+      }
+    }
+    for (e in twoMap) {
+      if (
+        twoMap[e].src ===
+        `https://rettungssimulator.online/images/marker/missions/${icon}_2.png`
+      ) {
+        twoMap[e].src = `${config_parse[i]["2"]}`;
+      }
+    }
+    for (e in threeMap) {
+      if (
+        threeMap[e].src ===
+        `https://rettungssimulator.online/images/marker/missions/${icon}_3.png`
+      ) {
+        threeMap[e].src = `${config_parse[i]["3"]}`;
+      }
+    }
+  }
+}
+
+socket.on("missionStatus", (missionStatusObject) => {
+  replaceIcons(missionStatusObject.icon);
+});
+
+socket.on("newMission", (missionObject) => {
+  console.log(missionObject.icon);
+  replaceIcons(missionObject.icon);
+});
+
+window.onload = function load() {
+  replaceIcons();
+};
