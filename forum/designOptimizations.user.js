658c515c77a2bbae545e7e37c34eea31113174c7
TheKeineAhnung <fireball73@gmx.de>
2021-12-18 11:32:19 +0100

    ✨ started working on UI

--- forum/designOptimizations.user.js
+++ forum/designOptimizations.user.js
@@ -25,7 +25,110 @@ function pageHeaderBackgroundImage(
   pageHeader.style.backgroundImage = `url(${imageUrl})`;
 }
 
+function settingsUITrigger() {
+  let settingsWheel = document.querySelector(".userPanelItems");
+  let settingsButton = document.createElement("li");
+  settingsButton.classList.add("designSettings");
+  let settingsLink = document.createElement("a");
+  settingsLink.href = "#";
+  settingsLink.id = "designSettings";
+  settingsLink.setAttribute("data-tooltip", "Design Settings");
+  let settingsSpan = document.createElement("span");
+  let settingsSpanText = document.createElement("span");
+  settingsSpanText.innerText = "Design Settings";
+  settingsSpan.classList.add("icon", "icon32", "fa-cog");
+  settingsLink.appendChild(settingsSpan);
+  settingsButton.appendChild(settingsLink);
+  settingsWheel.appendChild(settingsButton);
+
+  settingsLink.addEventListener("click", () => {
+    toggleUiDisplay();
+  });
+}
+
+function settingsUi() {
+  let body = document.querySelector("body");
+  let settingsMenu = document.createElement("div");
+  settingsMenu.id = "designSettingsMenu";
+  settingsMenu.style.position = "absolute";
+  settingsMenu.style.width = "90%";
+  settingsMenu.style.height = "auto";
+  settingsMenu.setAttribute("aria-hidden", "true");
+  settingsMenu.classList.add("dialogContainer");
+  settingsMenu.setAttribute("role", "dialog");
+  let settingsUiContent = giveSettingsUiContent();
+  settingsMenu.innerHTML = `
+    <header>
+      <span class="dialogTitle">Design Einstellungen</span>
+      <a class="dialogCloseButton" id="designSettingsMenuClose" aria-label="Schließen" data-tooltip="Schließen">
+        <span class="icon icon24 fa-times"></span>
+      </a>
+    </header>
+    <div class="dialogContent" style="height: 100%;">
+      <div class="designSettingsMenuContentItem">
+        <div class="designSettingsMenuContentItemContent" style="display: grid: grit">
+          ${settingsUiContent}
+        </div>
+      </div>
+    </div>
+    `;
+  body.appendChild(settingsMenu);
+  document
+    .querySelector("#designSettingsMenuClose")
+    .addEventListener("click", () => {
+      toggleUiDisplay();
+    });
+}
+
+// Function returns a dictionary with the following keys: "funtionName", "functionDescription"
+function settingsUiContent() {
+  return {
+    1: {
+      title: "Blaue Linien entfernen",
+      description: "Remove the blue line of category headers.",
+    },
+    2: {
+      title: "Hintergrundbild des Seitenheaders ändern",
+      description: "Change the background image of the page header.",
+    },
+  };
+}
+
+// Add the settings UI to the settings popup
+function giveSettingsUiContent() {
+  var content = settingsUiContent();
+  var finalContent = "";
+  for (var e in content) {
+    finalContent += addSettingsUiContentTemplateCard(
+      content[e].title,
+      content[e].description
+    );
+  }
+  return finalContent;
+}
+
+function addSettingsUiContentTemplateCard(title, desc) {
+  return `
+    <div class="designSettingsMenuContentItem" data-name="${title}" style="border: 2px solid rgba(161, 17, 22, 1); border-radius: 15px; margin-top: 0.25rem;">
+      <header style="font-size: 1.5rem; background-color: rgba(161, 17, 22, 1); padding: 0.25rem 2rem; border-top-left-radius: 11px; border-top-right-radius: 11px;">${title}</header>
+      <div class="designSettingsCardDescription" style="padding: 0.25rem 2rem;">${desc}</div>
+    </div>
+  `;
+}
+
+function toggleUiDisplay() {
+  let settingsUiElement = document.querySelector("#designSettingsMenu");
+  if (!settingsUiElement.classList.contains("active")) {
+    settingsUiElement.setAttribute("aria-hidden", "false");
+    settingsUiElement.classList.add("active");
+  } else {
+    settingsUiElement.setAttribute("aria-hidden", "true");
+    settingsUiElement.classList.remove("active");
+  }
+}
+
 window.addEventListener("load", function () {
   removeBlueLineOfCategoryHeaders();
-  pageHeaderBackgroundImage();
+  settingsUi();
+  settingsUITrigger();
 });
