387fce735807ab85a0cace8c967490a7e02f58e1
TheKeineAhnung <fireball73@gmx.de>
2022-01-13 20:49:34 +0100

    ⚡ added cache for userdata

--- chatMessageAlert.user.js
+++ chatMessageAlert.user.js
@@ -1,27 +1,32 @@
 // ==UserScript==
-// @name         ChatMessageAlert
+// @name         ChatMessageAlertGongBeta
 // @namespace    http://tampermonkey.net/
-// @version      1.1.2
+// @version      1.1.3
 // @description  Alert when new Message is in the Chat
 // @author       KeineAhnung
 // @run-at       document-end
-// @include      https://rettungssimulator.online/
+// @include      https://beta.rettungssimulator.online/
 // @updateURL    https://github.com/TheKeineAhnung/ReSi-Scripte/raw/main/chatMessageAlert.user.js
 // @downloadURL  https://github.com/TheKeineAhnung/ReSi-Scripte/raw/main/chatMessageAlert.user.js
 // ==/UserScript==
 
-async function chatMessageAlert() {
-  $.ajax({
-    url: "/api/user",
-    dataType: "json",
-    type: "GET",
-    success: function (r) {
-      sessionStorage.setItem("userName", r.userName);
-    },
-  });
-  var audio = new Audio("");
+if (
+  !localStorage.aUser ||
+  JSON.parse(localStorage.aUser).lastUpdate <
+    new Date().getTime() - 5 * 1000 * 60
+)
+  $.getJSON("/api/user").done((data) =>
+    localStorage.setItem(
+      "aUser",
+      JSON.stringify({ lastUpdate: new Date().getTime(), value: data })
+    )
+  );
+
+function chatMessageAlert() {
+  const username = JSON.parse(localStorage.aUser).value.userName;
+  var audio = new Audio("https://voca.ro/1jPC1Pp2JEkr");
   socket.on("associationMessage", (messageObject) => {
-    if (messageObject.userName != sessionStorage.getItem("userName")) {
+    if (messageObject.userName != username) {
       audio.play();
       var icons = document.querySelectorAll("link[rel~='icon']");
       var iconFirst = "images/favicons/favicon-32x32.png?v=WGLwy6Y942";
