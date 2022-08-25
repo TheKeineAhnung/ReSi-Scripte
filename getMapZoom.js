36f328f7d22cf106723c9c61efd2d8cc844af9fb
TheKeineAhnung <fireball73@gmx.de>
2022-08-25 16:28:32 +0200

    ✨ added script

---
+++ getMapZoom.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Get map zoom
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @run-at       document-end
+// @description  customize your mission Icons
+// @author       KeineAhnung
+// @include      https://rettungssimulator.online/*
+// @grant        none
+// ==/UserScript==
+
+const parentContainer = document.querySelector("div.leaflet-control-zoom");
+const currentZoomLevel = mymap.getZoom();
+const infoElement = document.createElement('a')
+infoElement.href = '#';
+infoElement.id = 'currentMapZoomLEvel';
+infoElement.style.cursor = "default"
+infoElement.style.fontWeight = "700"
+infoElement.dataset.tooltip = "Zoomlevel der Karte"
+infoElement.textContent = currentZoomLevel;
+
+parentContainer.insertAdjacentElement("beforeend", infoElement);
+
+const updateZoomLevel = function () {
+  const currentZoomLevel = mymap.getZoom();
+  document.querySelector("#currentMapZoomLevel")?.textContent = currentZoomLevel;
+};
+
+mymap.on("zoom", updateZoomLevel);
