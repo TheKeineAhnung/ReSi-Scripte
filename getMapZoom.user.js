2a9dd6bc111e16bd53d8ebd996cbcffe73a8b7b2
TheKeineAhnung <fireball73@gmx.de>
2022-08-25 23:55:17 +0200

    🐛 fixed zoom level update

--- getMapZoom.user.js
+++ getMapZoom.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Get map zoom
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @run-at       document-end
 // @description  customize your mission Icons
 // @author       KeineAhnung
@@ -11,19 +11,19 @@
 
 const parentContainer = document.querySelector("div.leaflet-control-zoom");
 const currentZoomLevel = mymap.getZoom();
-const infoElement = document.createElement('a')
-infoElement.href = '#';
-infoElement.id = 'currentMapZoomLEvel';
-infoElement.style.cursor = "default"
-infoElement.style.fontWeight = "700"
-infoElement.dataset.tooltip = "Zoomlevel der Karte"
+const infoElement = document.createElement("a");
+infoElement.href = "#";
+infoElement.id = "currentMapZoomLEvel";
+infoElement.style.cursor = "default";
+infoElement.style.fontWeight = "700";
+infoElement.dataset.tooltip = "Zoomlevel der Karte";
 infoElement.textContent = currentZoomLevel;
 
 parentContainer.insertAdjacentElement("beforeend", infoElement);
 
 const updateZoomLevel = function () {
   const currentZoomLevel = mymap.getZoom();
-  document.querySelector("#currentMapZoomLevel")?.textContent = currentZoomLevel;
+  document.querySelector("#currentMapZoomLevel").textContent = currentZoomLevel;
 };
 
 mymap.on("zoom", updateZoomLevel);
