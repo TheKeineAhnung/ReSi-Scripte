2b65c0d7d3087d60b85836ea76580534b2fd117c
KeineAhnung <kontakt@keineahnung.eu>
2024-11-22 13:28:35 +0100

    ✨ better mission status icon

--- script/updateMissionIconLinks.py
+++ script/updateMissionIconLinks.py
@@ -29,7 +29,7 @@ def writefile(content):
 
 def build_link(icon_name):
     icon_link_start = "https://rettungssimulator.online/images/marker/missions/"
-    icon_link_end = "_0.svg"
+    icon_link_end = "_1.svg"
     return icon_link_start + icon_name + icon_link_end
 
 def parse_json(json_data):
